a13876d19caad71edc02a10dd51ae01ecb46dec5
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2022-12-01 15:54:35 +0200

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API is running ...');
+});
+
+app.listen(PORT, console.log(`Server running on PORT: ${PORT}`));
